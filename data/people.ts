71b75c90112f4e94bdd89cae0788c0dcdde17ce3
Thai <minh.thai.tran@mail.mcgill.ca>
2026-02-25 20:51:45 -0500

    add faces for Joshua and Ben

--- data/people.ts
+++ data/people.ts
@@ -23,7 +23,7 @@ export const leadership: Person[] = [
   Person('Thai Tran', 'President', "I max out my Claude Pro usage at least twice a day", 'assets/people/ThaiTran.png', '', 'https://www.linkedin.com/in/thai-tran-minh/', 'https://github.com/thaimtl/'),
   Person('Ethan Tran', 'VP Operations', "BOULDERING🧗🧗‍♀️🧗‍♂️‼️‼️‼️‼️", 'assets/people/ethan.png', '', 'https://www.linkedin.com/in/-ethantran-/', 'https://github.com/tran-ethan'),
   Person('Virgile Couture', 'VP Finance', "I am still a 6-hcp golfer on paper", 'assets/people/VirgileCouture.png', '', 'https://www.linkedin.com/in/v-couture/'),
-  Person('Benjamin Ghaderi', 'Technical Director', "I like to build, and eat. I really love food.", 'assets/people/placeholder.png', 'benjamin.ghaderi@mail.mcgill.ca', 'https://linkedin.com/in/ben-ghaderi', 'https://github.com/BenGhad/'),
+  Person('Benjamin Ghaderi', 'Technical Director', "I like to build, and eat. I really love food.", 'assets/people/BenGhad.jpg', 'benjamin.ghaderi@mail.mcgill.ca', 'https://linkedin.com/in/ben-ghaderi', 'https://github.com/BenGhad/'),
   Person('Benjamin Coriat', 'Director of Partnerships and Sponsorships', "My superpower is memory and contacts! I remember most people and have no social awareness. Don't hesitate to reach out!", 'assets/people/BenjiCoriat.png', '', 'https://www.linkedin.com/in/benjamin-coriat-763a9819a/'),
   Person('Lawrence Kang', 'Director of Partnerships and Sponsorships', "Tagline of what you'll do", 'assets/people/LawrenceKang.jpeg'),
 ];
@@ -35,7 +35,7 @@ export const associates: Person[] = [
   Person('David Tang', 'General Associate', "Tagline of what you'll do", 'assets/people/DavidTang.jpeg'),
   Person('Mubeen Mohammed', 'General Associate', "I am a U3 Computer Science student with an interest in ML and AI. I play squash and read manga in my free time", 'assets/people/MubeenMohammed.jpeg', 'mubeen.mohammed@mail.mcgill.ca', 'https://www.linkedin.com/in/mubeen12', 'https://github.com/mubeenmohammed'),
   Person('Minh Vo', 'General Associate', "Tagline of what you'll do", 'assets/people/MinhVo.jpeg'),
-  Person('Joshua', 'General Associate', "Tagline of what you'll do", 'assets/people/placeholder.png'),
+  Person('Joshua', 'General Associate', "Tagline of what you'll do", 'assets/people/joshua.jpg'),
   Person('Julien Yang', 'General Associate', "Tagline of what you'll do", 'assets/people/JulienYang.jpeg'),
   Person('Parsa Rahimnia', 'General Associate', "Claude University Alumni with a Bachelors in prompt engineering", 'assets/people/ParsaRahimnia.jpeg', '', '', ' https://github.com/ParseDotEXE'),
   Person('Ralph Azrak', 'General Associate', "Software Engineering student at McGill with a strong interest in product development and emerging technologies. As CBC Hackathon Team Lead, I coordinate teams and ensure we deliver a high-impact and well-organized event", 'assets/people/RalphAzrak.jpg', 'ralph.azrak@mail.mcgill.ca', 'www.linkedin.com/in/ralph-azrak/', 'https://github.com/RalphAzrak'),
